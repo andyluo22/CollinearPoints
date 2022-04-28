390f6fb20cd169d92f948799712e78a18eac7ce4
Andyl <andy.luo22@hotmail.com>
2022-04-27 17:26:24 -0700

    Finished Point Data Type and Attempted BruteCollinearPoints class.

--- src/BruteCollinearPoints.java
+++ src/BruteCollinearPoints.java
@@ -1,17 +1,66 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
 public class BruteCollinearPoints {
+    private int numOfSegments;
+    private LineSegment[] segmentsTwoEndpoints;
 
-    // finds all line segments containing 4 points
+    // finds all line segments containing 4 points using brute force
     public BruteCollinearPoints(Point[] points) {
+        for(int i = 0; i < points.length; i++) {
+            for(int j = i + 1; j < points.length; j++) {
+                for (int k = j + 1; k < points.length; j++) {
+                    for(int l = k + 1; l < points.length; l++) {
+                        Double slope1 = points[i].slopeTo(points[j]);
+                        Double slope2 = points[i].slopeTo(points[k]);
+                        Double slope3 = points[i].slopeTo(points[l]);
 
+                        if(slope1 == slope2 && slope2 == slope3) { //we are using equivalent relations here specifically transitivity Math 220
+                            numOfSegments++;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     // the number of line segments
     public int numberOfSegments() {
-        return 0;
+        return this.numOfSegments;
     }
 
     // the line segments
     public LineSegment[] segments() {
         return null;
+    }
+
+    public static void main(String[] args) {
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        FastCollinearPoints collinear = new FastCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
     }
 }
--- src/Point.java
+++ src/Point.java
@@ -5,14 +5,13 @@ public class Point implements Comparable<Point> {
 
     private final int x;     // x-coordinate of this point
     private final int y;     // y-coordinate of this point
-    public static final Comparator<Point>  BY_SLOPE = new BySlope();
-
+    public final Comparator<Point> BY_SLOPE = new BySlope(); //One Comparator per Ordering method (by slope for this comparator)
 
     /**
      * Initializes a new point.
      *
-     * @param  x the <em>x</em>-coordinate of the point
-     * @param  y the <em>y</em>-coordinate of the point
+     * @param x the <em>x</em>-coordinate of the point
+     * @param y the <em>y</em>-coordinate of the point
      */
     public Point(int x, int y) {
         this.x = x;
@@ -44,7 +43,7 @@ public class Point implements Comparable<Point> {
      * Double.POSITIVE_INFINITY if the line segment is vertical;
      * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
      *
-     * @param  that the other point
+     * @param that the other point
      * @return the slope between this point and the specified point
      */
     public double slopeTo(Point that) {
@@ -59,18 +58,18 @@ public class Point implements Comparable<Point> {
      * Formally, the invoking point (x0, y0) is less than the argument point
      * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
      *
-     * @param  that the other point
+     * @param that the other point
      * @return the value <tt>0</tt> if this point is equal to the argument
-     *         point (x0 = x1 and y0 = y1);
-     *         a negative integer if this point is less than the argument
-     *         point; and a positive integer if this point is greater than the
-     *         argument point
+     * point (x0 = x1 and y0 = y1);
+     * a negative integer if this point is less than the argument
+     * point; and a positive integer if this point is greater than the
+     * argument point
      */
     public int compareTo(Point that) {
-        if(this.y < that.y) return -1;
-        if(this.y > that.y) return +1;
-        if(this.x < that.x) return -1;
-        if(this.x > that.x) return +1;
+        if (this.y < that.y) return -1;
+        if (this.y > that.y) return +1;
+        if (this.x < that.x) return -1;
+        if (this.x > that.x) return +1;
         return 0; // they are the same points
     }
 
@@ -81,13 +80,18 @@ public class Point implements Comparable<Point> {
      * @return the Comparator that defines this ordering on points
      */
     public Comparator<Point> slopeOrder() {
-        return null;
+        return BY_SLOPE;
     }
 
-    private static class BySlope implements Comparator<Point> {
-        public int compare(Point )
-    }
+    private class BySlope implements Comparator<Point> {
+        public int compare(Point firstPoint, Point secondPoint) {
+            Point that = new Point(x, y);
+            Double firstToInvokingSlope = firstPoint.slopeTo(that);
+            Double secondToInvokingSlope = secondPoint.slopeTo(that);
 
+            return Double.compare(firstToInvokingSlope,secondToInvokingSlope);
+        }
+    }
 
 
     /**
@@ -105,5 +109,38 @@ public class Point implements Comparable<Point> {
      * Unit tests the Point data type.
      */
     public static void main(String[] args) {
-        /* YOUR CODE HERE */
+        Point point1 = new Point(1, 2);
+        Point point2 = new Point(2, 3);
+
+        Point point3 = new Point(2, 2);
+        Point point4 = new Point(2, 3);
+
+        Point point5 = new Point(2, 2);
+        Point point6 = new Point(2, 2);
+
+        Point point7 = new Point(3, 2);
+        Point point8 = new Point(2, 2);
+
+        System.out.println("Testing slopeTo method of a finite slope length");
+        System.out.println(point1.slopeTo(point2));
+
+        System.out.println("Testing slopeTo method of an infinite slope length");
+        System.out.println(point3.slopeTo(point4));
+
+        System.out.println("Testing slopeTo method of an negative infinite slope length - points are the same");
+        System.out.println(point5.slopeTo(point6));
+
+        System.out.println("Testing slopeTo method of a 0 slope length");
+        System.out.println(point7.slopeTo(point8));
+
+        System.out.println("Testing Comparator comparing by slope - positive number if point 1 is further from invoking point and negative if closer and 0 if the same");
+        System.out.println(point1.BY_SLOPE.compare(point2,point3));
+        System.out.println(point1.BY_SLOPE.compare(point3,point2));
+        System.out.println(point1.BY_SLOPE.compare(point2,point2));
+
+        System.out.println("Testing slope Order method without passing in comparator instance and just calling slopeOrder method");
+        System.out.println(point1.slopeOrder().compare(point1,point2));
+
+
     }
+}
