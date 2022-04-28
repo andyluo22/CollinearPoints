1f65f897229c9e48da9a395268461ab9c5b1baf5
Andyl <andy.luo22@hotmail.com>
2022-04-27 21:42:50 -0700

    Finished BruteCollinearPoints Class without testing in sample client

--- src/BruteCollinearPoints.java
+++ src/BruteCollinearPoints.java
@@ -1,22 +1,56 @@
+import com.sun.org.apache.bcel.internal.generic.ARRAYLENGTH;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
+import javax.sound.sampled.Line;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class BruteCollinearPoints {
     private int numOfSegments;
-    private LineSegment[] segmentsTwoEndpoints;
+    private ArrayList<LineSegment> segmentsWithTwoEndpoints;
 
     // finds all line segments containing 4 points using brute force
     public BruteCollinearPoints(Point[] points) {
-        for(int i = 0; i < points.length; i++) {
-            for(int j = i + 1; j < points.length; j++) {
+        Point origin = new Point(0, 0);
+
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
                 for (int k = j + 1; k < points.length; j++) {
-                    for(int l = k + 1; l < points.length; l++) {
+                    for (int l = k + 1; l < points.length; l++) {
                         Double slope1 = points[i].slopeTo(points[j]);
                         Double slope2 = points[i].slopeTo(points[k]);
                         Double slope3 = points[i].slopeTo(points[l]);
 
-                        if(slope1 == slope2 && slope2 == slope3) { //we are using equivalent relations here specifically transitivity Math 220
+                        if (slope1 == slope2 && slope2 == slope3) { //we are using equivalent relations here specifically transitivity Math 220
+                            Double slope1ToOrigin = origin.slopeTo(points[i]);
+                            Double slope2ToOrigin = origin.slopeTo(points[j]);
+                            Double slope3ToOrigin = origin.slopeTo(points[k]);
+                            Double slope4ToOrigin = origin.slopeTo(points[l]);
+
+                            ArrayList<Point> storePoints = new ArrayList<Point>();
+                            storePoints.add(points[i]);
+                            storePoints.add(points[j]);
+                            storePoints.add(points[k]);
+                            storePoints.add(points[l]);
+
+                            ArrayList<Double> storeSlopes = new ArrayList<Double>();
+
+                            storeSlopes.add(slope1ToOrigin);
+                            storeSlopes.add(slope2ToOrigin);
+                            storeSlopes.add(slope3ToOrigin);
+                            storeSlopes.add(slope4ToOrigin);
+
+                            Double max = Collections.max(storeSlopes);
+                            Double min = Collections.min(storeSlopes);
+
+                            int maxIndex = storeSlopes.indexOf(max);
+                            int minIndex = storeSlopes.indexOf(min);
+
+                            LineSegment line = new LineSegment(storePoints.get(maxIndex), storePoints.get(minIndex));
+                            this.segmentsWithTwoEndpoints.add(line);
                             numOfSegments++;
                         }
                     }
@@ -32,7 +66,13 @@ public class BruteCollinearPoints {
 
     // the line segments
     public LineSegment[] segments() {
-        return null;
+        LineSegment[] lineSegments = new LineSegment[this.numOfSegments];
+
+        for (int i = 0; i < this.numOfSegments; i++) {
+            lineSegments[i] = segmentsWithTwoEndpoints.get(i);
+        }
+
+        return lineSegments;
     }
 
     public static void main(String[] args) {
